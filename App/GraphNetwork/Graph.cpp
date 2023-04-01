55225055b2333a79fc54b2e016624d92c8352632
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-04-01 14:32:28 +0300

    Graph.cpp

---
+++ App/GraphNetwork/Graph.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "inputData.cpp"
+
+int main()
+{
+    Graph <unsigned long long> GitHub(7624, 27.806);
+    GitHub.inputData();
+    //std::cout << GitHub;
+    GitHub.printShortestDistance(2, 870);
+    //GitHub.countAndPrintConnectedComponents();
+    //GitHub.printEulerianCycles();
+    //GitHub.printMST();
+    GitHub.printAllHamiltonianCycles();
+    return 0;
+}
