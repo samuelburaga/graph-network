31de5b58d6d235a27cf9b81046aaae3c26b32d42
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-04-01 21:38:43 +0300

    Create graph network

--- App/Graph network/Graph network.py
+++ App/Graph network/Graph network.py
@@ -4,3 +4,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 data = pd.read_csv("Resources/lasftm_asia/lastfm_asia_edges.csv")
 print(data)
+G = nx.Graph()
+for index, row in data.iterrows():
+    G.add_edge(row['node_1'], row['node_2'])
+nx.draw_spring(G, with_labels = True, node_size = 400)
+plt.show()
