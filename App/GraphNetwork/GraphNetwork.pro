1a8e840014c037b36581484477ce90baa68eda41
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-04-01 14:30:06 +0300

    inputData .h to .cpp

--- App/GraphNetwork/GraphNetwork.pro
+++ App/GraphNetwork/GraphNetwork.pro
@@ -5,6 +5,7 @@ QT += quick
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+        inputData.cpp \
         main.cpp
 
 RESOURCES += qml.qrc
---
+++ App/GraphNetwork/inputData.cpp
@@ -0,0 +1,39 @@
+#include "Graph.h"
+#include "stringToType.h"
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+template <class Type> void Graph <Type>::inputData()
+{
+    try
+    {
+        std::ifstream file;
+        //file.open("D:/ULBS/Anul II/Semestrul II/Modulul 1/Algoritmica grafurilor/Project/Coding/Social network of GitHub developers/Resources/git_web_ml/musae_git_edges.csv");
+        //file.open("D:/ULBS/Anul II/Semestrul II/Modulul 1/Algoritmica grafurilor/Project/Coding/Social network of GitHub developers/Resources/soc-sign-bitcoinalpha/soc-sign-bitcoinalpha.csv");
+        file.open("D:/ULBS/Anul II/Semestrul II/Modulul 1/Algoritmica grafurilor/Project/Coding/Social network of GitHub developers/Resources/lasftm_asia/lastfm_asia_edges.csv");
+        std::string titles;
+        getline(file, titles);
+        std::string line;
+        unsigned long long edge = 0;
+        while (getline(file, line) /* && edge != (*this).vertices */)
+        {
+            std::istringstream ss(line);
+            std::string firstColumn, secondColumn;
+            getline(ss, firstColumn, ',');
+            getline(ss, secondColumn, ',');
+            int num1 = stoi(firstColumn);
+            int num2 = stoi(secondColumn);
+            Type u = convert_to <Type>(firstColumn);
+            Type v = convert_to <Type>(secondColumn);
+            edge++;
+            (*this).addEdge(u, v, edge);
+        }
+    }
+    catch (...)
+    {
+        throw new std::exception("File error!");
+    }
+}
