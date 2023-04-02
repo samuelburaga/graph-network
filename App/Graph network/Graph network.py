9bbb1394c536387cdde904740ca9909a1074a068
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-04-02 10:43:21 +0300

    All types of graph data structures

--- App/Graph network/Graph network.py
+++ App/Graph network/Graph network.py
@@ -9,18 +9,38 @@ data = pd.read_csv("Resources/lasftm_asia/lastfm_asia_edges.csv")
 G = nx.Graph()
 for index in range(7624):
     G.add_node(index)
+# i = 1
 for index, row in data.iterrows():
     G.add_edge(row['node_1'], row['node_2'])
+    # if i > 10:
+    #     break
+    # i = i + 1
 node_colors = {}
 for node in G.nodes():
     node_colors[node] = "#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
 nx.draw(G, with_labels = True, node_size = 400, node_color = list(node_colors.values()), font_size = 6)
-
-# print(nx.adjacency_matrix(G))
 # Get the adjacency matrix as a pandas DataFrame
 adjacency_matrix = pd.DataFrame(nx.adjacency_matrix(G).todense())
-
 # Export the adjacency matrix to a CSV file
 adjacency_matrix.to_csv('Output/adjacency_matrix.csv', index = False, header = False)
 
+incidence_matrix = nx.incidence_matrix(G, nodelist=None, edgelist=None, oriented=False, weight=None).todense()
+
+df = pd.DataFrame(incidence_matrix, columns=[e for e in G.edges()])
+
+# Add a column with the node numbers
+df.insert(0, 'Node', [n for n in G.nodes()])
+
+df.to_csv('Output/incidence_matrix.csv', index=False)
+
+# get the adjacency list as a dictionary
+adjacency_list = nx.to_dict_of_lists(G)
+
+# write the adjacency list to a CSV file
+with open('Output/adjacency_list.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Node', 'Neighbors'])
+    for node, neighbors in adjacency_list.items():
+        writer.writerow([node] + neighbors)
+        
 plt.show()
